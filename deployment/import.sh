40aa7be83699048e7c1f8158a54b6412589d2ecb
mspopova <popova@ip-bt.ru>
2020-01-23 01:31:30 +0300

    bad

--- deployment/import.sh
+++ deployment/import.sh
@@ -2,6 +2,8 @@
 
 ./stop.sh
 docker volume rm skolboard-postgres-dbdata
-docker run -v skolboard-postgres-dbdata:/var/lib/postgresql/data:rw -v ./dumps/:/dumps:rw --name tmp --rm -d postgres:latest
-docker exec -it tmp sh -c "chown -R postgres /dumps && runuser postgres psql -c 'CREATE DATABASE skoltech_board_db;'"
+DUMP_PATH=$(realpath ./dumps)
+docker run -v skolboard-postgres-dbdata:/var/lib/postgresql/data:rw -v "$DUMP_PATH":/dumps:rw --name tmp --rm -d postgres:latest
+docker exec -it tmp chown -R postgres /dumps
+#docker exec -it tmp runuser postgres sh -c "psql -c 'CREATE DATABASE skoltech_board_db;'"
 docker exec -it tmp runuser postgres psql -d skoltech_board_db -f /dumps/init.sql
