8eed249636e059066c2eaf07394db1d5b3d3b1f6
aynur <maksutov_aynur@mail.ru>
2020-01-23 10:33:23 +0300

    return profile

--- board/models/post.py
+++ board/models/post.py
@@ -2,7 +2,7 @@ from django.db import models as M
 from board.models.profile import Profile, ProfileSerializer
 from board.models.tag import Tag, TagSerializer
 from board.enums import ReactionType, mk_choices
-from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField, ModelField
 
 
 class Post(M.Model):
@@ -18,13 +18,13 @@ class Post(M.Model):
 
 class PostSerializer(ModelSerializer):
     tags = TagSerializer(many=True)
-    profile = ProfileSerializer(read_only=True)
-    profile_id = PrimaryKeyRelatedField(source=profile, write_only=True)
+    profile = ProfileSerializer(read_only=True, many=False)
+    profile_id = PrimaryKeyRelatedField(write_only=True, queryset=Profile.objects.all(), source='profile')
 
     class Meta:
         model = Post
         fields = ['id', 'created_dttm', 'profile', 'profile_id', 'title', 'body', 'tags']
-        read_only_fields = ('id', 'created_dttm', 'tags')
+        read_only_fields = ('id', 'created_dttm', 'tags', 'profile')
 
 
 class Reaction(M.Model):
